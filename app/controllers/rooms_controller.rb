fb9bb87775ff034d505cef8815df205f104f35fc
Mohammed Karim Zinoun <zinoun.mkarim@gmail.com>
2022-08-30 17:57:37 +0200

    project and rooms creation

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,16 +5,31 @@ class RoomsController < ApplicationController
   def show
   end
 
+  def new
+    @project = Project.find(params[:project_id])
+    @room = Room.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    list = params["room"]["room_type"]
+    clean_list = list[1..list.length - 1]
+    clean_list.each do |room|
+      @room = Room.new(room_type: room)
+      @room.project = @project
+      @room.photos = params["room"]["photos"]
+      if @room.save
+        redirect_to project_path(@project)
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
+
   def edit
   end
 
   def update
-  end
-
-  def new
-  end
-
-  def create
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :projects do
     resources :participations, only: %i[create destroy]
-    resources :rooms, only: %i[create update destroy]
+    resources :rooms, only: %i[show new create update destroy]
   end
   resources :rooms, only: [] do
     resources :tasks, only: %i[create update destroy]

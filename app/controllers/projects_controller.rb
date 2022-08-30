406c6d9c8083529353fccb321ec529ccae25a9cb
Ariel Jurkevitch <arieljurkevitch@gmail.com>
2022-08-30 18:01:50 +0200

    Dashboard begin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,11 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+    # if params[:query].present?
+    #   @projects = policy_scope(Project).search_custom_project(params[:query])
+    # else
+    #   @projects = policy_scope(Project)
+    # end
   end
 
   def show

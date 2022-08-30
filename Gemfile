ba8c7f582f42fc7acaef0fb6066097477bdb1eb6
matteobianchi <matteo.bianchi@hec.edu>
2022-08-30 12:02:47 +0200

    Seed created, Cloudinary and Active Record initiated

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby "3.1.2"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
 gem "dotenv-rails", groups: [:development, :test]
-
+gem "cloudinary"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_094025) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_095751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,7 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_094025) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string "type"
+    t.string "room_type"
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

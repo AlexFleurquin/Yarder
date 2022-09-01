86884dd151290ac84b4b46b0df1e9566f0261291
Ariel Jurkevitch <arieljurkevitch@gmail.com>
2022-09-01 17:38:20 +0200

    button delete room and resize first card

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,8 +5,8 @@ class RoomsController < ApplicationController
 
   def show
     @room = Room.find(params[:id])
-    @tasks = @room.tasks
     @task = Task.new
+    @tasks = @room.tasks
   end
 
   def new

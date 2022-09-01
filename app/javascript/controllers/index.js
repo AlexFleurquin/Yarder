323598ea85ac0c32ec68877c2eac09a4d60b5d50
matteobianchi <matteo.bianchi@hec.edu>
2022-09-01 17:37:05 +0200

    full rework of add project form and some other front end items

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,3 +15,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import FormScrollController from "./form_scroll_controller"
+application.register("form-scroll", FormScrollController)

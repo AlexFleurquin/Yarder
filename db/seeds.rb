0acc02fd908a14610362d350c30655a17eb12958
matteobianchi <matteo.bianchi@hec.edu>
2022-09-02 14:28:27 +0200

    small changes before pushing to prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ Participation.destroy_all
 Room.destroy_all
 Project.destroy_all
 User.destroy_all
+Task.destroy_all
 
 puts "Creating 2 users, 1 project, 2 rooms, 3 photos for each room and 1 participation..."
 

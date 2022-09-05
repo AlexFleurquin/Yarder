d7a38ab28a06a5db5ddb2cad4ca691ce1367af74
Ariel Jurkevitch <arieljurkevitch@gmail.com>
2022-09-05 17:48:49 +0200

    add artisan to my team

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -6,6 +6,15 @@ class ParticipationsController < ApplicationController
   end
 
   def create
+    # recup le projet
+    @project = Project.find(params[:id])
+    # recup le user params..
+    @user = User.find(params[:id])
+    # participation.create (participation_params en private)
+    @participation.create(participation_params)
+    # if save redirect to project_path(@project, tab: 'member')  => /projects/15?tab=member
+    if @participation.save redirect_to project_path(@project, tab: 'member')
+    end
   end
 
   def destroy
@@ -13,9 +22,16 @@ class ParticipationsController < ApplicationController
 
   def artisans
     @artisans = User.artisans
+    @participation = Participation.new
   end
 
   def artisan_show
     @artisan = User.artisan(params[:id])
+  end
+
+  private
+
+  def participation_params
+    participation_params.require(@participation).permit(:user_id, :project_id)
   end
 end

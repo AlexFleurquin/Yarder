5505e5a99a4c60a418787c113f12d041c1fccc64
Alexandre Fleurquin <alexandre.fleurquin@gmail.com>
2022-09-05 14:47:00 +0200

    Oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  devise :omniauthable, omniauth_providers: [:facebook]
+  devise :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   has_many :projects
   has_many :participations

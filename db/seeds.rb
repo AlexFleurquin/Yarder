ebb4d4116ae5e70713fdeb8027c01c6897e59505
Mohammed Karim Zinoun <zinoun.mkarim@gmail.com>
2022-09-05 17:48:07 +0200

    first version of chatroom front

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,18 @@ Room.destroy_all
 Project.destroy_all
 User.destroy_all
 
-puts "Creating 2 users, 1 project, 2 rooms, 2 tasks, 3 photos for each room and 1 participation..."
+puts "Creating 3 users, 1 project, 2 rooms, 2 tasks, 3 photos for each room and 2 participation..."
 
 
 user1 = User.new(first_name: 'Romain', last_name: 'Paillard', email: 'romain@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true, bio:"Electricien depuis 20 ans et issu d'une lignée de plombier depuis 1830, je me tiens volontaire pour vous dépanner et pour disrupter le marché du BTP")
 user2 = User.new(first_name: 'Boris', last_name: 'Paillard', email: 'boris@abc.com', password: 'azerty', phone_number: '0503050434')
+user3 = User.new(first_name: 'Paul', last_name: 'Portier', email: 'paul@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true)
 user1.photo.attach(io: URI.open("https://media-exp1.licdn.com/dms/image/C5603AQEXicEGcgYBOQ/profile-displayphoto-shrink_800_800/0/1517764268318?e=1666828800&v=beta&t=MAxLCV9i595HRSpuXCyYUtbv2ngZQofLUwppNVfkdvM") , filename: "user1.png", content_type: "image/png")
 user2.photo.attach(io: URI.open("https://media-exp1.licdn.com/dms/image/C4E03AQEfSGDe5SR0vQ/profile-displayphoto-shrink_400_400/0/1525092669872?e=1666828800&v=beta&t=TpAnM5Ewp40dPqYPVVOeANXZeuCrU-ls0jb4DilYn2c") , filename: "user2.png", content_type: "image/png")
+user3.photo.attach(io: URI.open("https://avatars.githubusercontent.com/u/54004476?v=4"), filename: "user3.png", content_type: "image/png")
 user1.save!
 user2.save!
+user3.save!
 
 project1 = Project.new(user_id: user2.id, name: 'Mon premier Projet', address: "16 villa Gaudelet, 75011 Paris", building_type: "Immeuble", construction_year: "1969", urgency: "Pas de date fixée", budget: 6900, description: "Charmant immeuble avec grande capacité")
 project1.photo.attach(io: URI.open("https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80") , filename: "Project1.png", content_type: "image/png")
@@ -38,5 +41,7 @@ task2.save!
 
 participation1 = Participation.create!(user_id: user1.id, project_id: project1.id)
 participation1.save!
+participation2 = Participation.create!(user_id: user3.id, project_id: project1.id)
+participation2.save!
 
 puts "Done!"

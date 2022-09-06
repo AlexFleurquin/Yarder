d4b4315faa778af64ae731aa3ebaa2a281705c20
Mohammed Karim Zinoun <zinoun.mkarim@gmail.com>
2022-09-06 16:27:39 +0200

    seed updated with 2 tasks assigned to 2 artisans

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@ Room.destroy_all
 Project.destroy_all
 User.destroy_all
 
-puts "Creating 5 users, 1 project, 2 rooms, 2 tasks, 3 photos for each room and 2 participation..."
+puts "Creating 5 users, 1 project, 2 rooms, 2 tasks, 3 photos for each room and 2 participations..."
 
 
-user1 = User.new(first_name: 'Romain', last_name: 'Paillard', email: 'romain@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true, bio:"Electricien depuis 20 ans et issu d'une lignée de plombier depuis 1830, je me tiens volontaire pour vous dépanner et pour disrupter le marché du BTP")
+user1 = User.new(first_name: 'Romain', last_name: 'Paillard', email: 'romain@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true, specialty: "Electricien", bio:"Electricien depuis 20 ans et issu d'une lignée de plombier depuis 1830, je me tiens volontaire pour vous dépanner et pour disrupter le marché du BTP")
 user2 = User.new(first_name: 'Boris', last_name: 'Paillard', email: 'boris@abc.com', password: 'azerty', phone_number: '0503050434')
-user3 = User.new(first_name: 'Paul', last_name: 'Portier', email: 'paul@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true)
+user3 = User.new(first_name: 'Paul', last_name: 'Portier', email: 'paul@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true, specialty: "Menuisier", bio: "Je travaille le bois depuis mes 6 ans")
 user4 = User.new(first_name: 'Diane', last_name: 'Roussillon', email: 'diane@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true, speciality: "Peintre", bio: "Je peins très bien et depuis longtemps")
 user5 = User.new(first_name: 'Paul', last_name: 'Lahana', email: 'paull@abc.com', password: 'azerty', phone_number: '0503050434', is_pro: true, speciality: "Plombier", bio: "Marre des étudiants qui me demandent de l'aide tout le temps je préfère me réorienter dans les tuyaux")
 
@@ -43,8 +43,8 @@ room2.photos.attach(io: URI.open("https://images.unsplash.com/photo-156454058324
 room1.save!
 room2.save!
 
-task1 = Task.new(name: "Peindre le plafond", description: "Peindre le plafond de la pièce en blanc", room_id: room1.id, status: "A faire", position: 1, start_date: "2022-09-01 00:00:00 UTC" , end_date: "2022-09-20 00:00:00 UTC")
-task2 = Task.new(name: "Mise en route compteur", description: "Mise en route compteur électrique", room_id: room1.id, status: "A faire", position: 2, start_date: "2022-09-10 00:00:00 UTC" , end_date: "2022-09-15 00:00:00 UTC")
+task1 = Task.new(name: "Peindre le plafond", description: "Peindre le plafond de la pièce en blanc", room_id: room1.id, status: "A faire", position: 1, start_date: "2022-09-01 00:00:00 UTC" , end_date: "2022-09-20 00:00:00 UTC", user_id: 3)
+task2 = Task.new(name: "Mise en route compteur", description: "Mise en route compteur électrique", room_id: room1.id, status: "A faire", position: 2, start_date: "2022-09-10 00:00:00 UTC" , end_date: "2022-09-15 00:00:00 UTC", user_id: 4)
 task1.save!
 task2.save!
 

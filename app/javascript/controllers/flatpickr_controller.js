d7cc8ef3fdd95f70a05159e64a9889657732e143
Alexandre Fleurquin <alexandre.fleurquin@gmail.com>
2022-09-07 15:05:28 +0200

    Chatroom style

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,12 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
-  connect() {
-    flatpickr(this.element, {
 
+  static targets = [ 'startDateInput', 'endDateInput' ]
+
+  connect() {
+    console.log("coucou");
+    flatpickr(this.startDateInputTarget, {
+      altInput: true,
+      mode: 'range',
+      "plugins": [new rangePlugin({ input: this.endDateInputTarget})]
     });
   }
 }

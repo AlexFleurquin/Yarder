50fe32638a6708b8771f5d0bd89226bbc7f49c20
Mohammed Karim Zinoun <zinoun.mkarim@gmail.com>
2022-09-07 18:05:45 +0200

    Both scroll are working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import ScrolldownConversationController from "./scrolldown_conversation_controller"
+application.register("scrolldown-conversation", ScrolldownConversationController)
+
 import AddressAutocompleteController from "./address_autocomplete_controller"
 application.register("address-autocomplete", AddressAutocompleteController)
 
